629a207fe4a3cfe9f049d379e782c216266af1e0
Pairot Peungnumsai <God_Buddha@outlook.co.th>
2019-01-01 00:03:17 +0700

    Test

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static 
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                int caseSwitch = 1;
+
+                switch (caseSwitch)
+                {
+                    case 1:
+                        
+                        break;
+                    case 2:
+                        
+                        break;
+                    default:
+                        Console.WriteLine(" Thank You ");
+                        break;
+                }
+            } while ( caseSwitc == 1);
+        }
+
+    }
+}
